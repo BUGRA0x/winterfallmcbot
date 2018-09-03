2dcdf44415eef6eec01ce4cbceec7713765dc5c3
BugraGunes <42942210+BugraGunes@users.noreply.github.com>
2018-09-03 22:23:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,111 +1,176 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const ayarlar = require('./ayarlar.json');
-const chalk = require('chalk');
-const fs = require('fs');
-const moment = require('moment');
-require('./util/eventLoader')(client);
+const prefix = '&';
 
-var prefix = ayarlar.prefix;
-
-const log = message => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
-};
-
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-fs.readdir('./komutlar/', (err, files) => {
-  if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
-  files.forEach(f => {
-    let props = require(`./komutlar/${f}`);
-    log(`Yüklenen komut: ${props.help.name}.`);
-    client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.help.name);
-    });
-  });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
 });
-
-client.reload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
-
-client.load = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
-
-client.unload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa') {
-		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
-			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
-		} else {
-		msg.reply('Aleyküm selam, hoş geldin ^^');
-		}
-	}
+  if (msg.content === prefix + 'yardım') {
+  	if (msg.author.bot) return;
+   	msg.reply('Yardım Sayfasına Git https://ugurkilci.github.io/RamazanBot/');
+  }
+
+  if (msg.content === 'acıktım') {
+   	if (Math.floor((Math.random() * 15) + 1) === 1) {
+   		msg.reply('Az sabret iftara az kaldı!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 2) {
+   		msg.reply('Sabreden deviş muradına ermiş!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 3) {
+   		msg.reply('Sabret kardeşim!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 4) {
+   		msg.reply('Film izle. Açlığını unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 5) {
+   		msg.reply('Dizi izle. Açlığını unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 6) {
+   		msg.reply('Oyun oyna. Açlığını unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 7) {
+   		msg.reply('Ders çalış açlığını unutursun! Açlığını unutursun. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 8) {
+   		msg.reply('Git bi gez gel. Açlığını unutursun. Güneşden gitmemeye çalış!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 9) {
+   		msg.reply('Geçecek bunlar, sen neler atlattın bu ne ki? :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 10) {
+   		msg.reply('Büyüyünce geçer. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 11) {
+   		msg.reply('Ağla. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 12) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki çatalı yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 13) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki kaşığı yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 14) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki bıçağı yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 15) {
+   		msg.reply('Başka bir şey düşünmeye çalış!');
+   	}
+  }
+
+  if (msg.content === 'susadım') {
+   	if (Math.floor((Math.random() * 15) + 1) === 1) {
+   		msg.reply('Az sabret iftara az kaldı!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 2) {
+   		msg.reply('Sabreden deviş muradına ermiş!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 3) {
+   		msg.reply('Sabret kardeşim!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 4) {
+   		msg.reply('Film izle. Susuzluğunu unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 5) {
+   		msg.reply('Dizi izle. Susuzluğunu unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 6) {
+   		msg.reply('Oyun oyna. Susuzluğunu unutursun. :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 7) {
+   		msg.reply('Ders çalış açlığını unutursun! Susuzluğunu unutursun. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 8) {
+   		msg.reply('Git bi gez gel. Susuzluğunu unutursun.');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 9) {
+   		msg.reply('Geçecek bunlar, sen neler atlattın bu ne ki? :)');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 10) {
+   		msg.reply('Büyüyünce geçer. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 11) {
+   		msg.reply('Ağla. :D');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 12) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki şişeyi yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 13) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki içeceği yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 14) {
+   		msg.reply('Lanet olsun dostum, hemen o elindeki bardağı yere bırak!');
+   	}else if (Math.floor((Math.random() * 15) + 1) === 15) {
+   		msg.reply('Başka bir şey düşünmeye çalış!');
+   	}
+  }
+
+  if (msg.content === 'of') {
+   	msg.reply('Oflama geçer bugünler!');
+  }
+
+  if (msg.content === 'ah') {
+   	msg.reply('Ah deme oh de!');
+  }
+
+  if (msg.content === 'oh') {
+   	msg.reply('Oh deme püf de!');
+  }
+
+  if (msg.content === 'püf') {
+   	msg.reply('Git, Barış MANÇO nun -Lambaya Püf De- dinle!');
+  }
+
+  if (msg.content === 'iftara ne kadar var') {
+   	msg.reply('Şimdi öğren -> https://iftaranekadarkaldi.com/');
+  }
+
+  if (msg.content === 'iftara kaç saat var') {
+   	msg.reply('Şimdi öğren -> https://iftaranekadarkaldi.com/');
+  }
+
+  if (msg.content === 'iftara kaç dakika var') {
+   	msg.reply('Şimdi öğren -> https://iftaranekadarkaldi.com/');
+  }
+
+  if (msg.content === 'iftara kaç gün var') {
+   	msg.reply('Yok devenin bale pabucu!?');
+  }
+
+  if (msg.content === 'iftar ne zaman') {
+  	if (Math.floor((Math.random() * 4) + 1) === 1) {
+   		msg.reply('Zamanı geldiği zaman!');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 2) {
+   		msg.reply('İmam uyumuş olmasın?');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 3) {
+   		msg.reply('İmam bayıldı(!)');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 4) {
+   		msg.reply('Biraz Google la! ;)');
+   	}
+  }
+
+  if (msg.content === 'iftara ne kadar kaldı') {
+  	if (Math.floor((Math.random() * 4) + 1) === 1) {
+   		msg.reply('Çok değil! www.com');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 2) {
+   		msg.reply('Görende 3 gün aç kaldı sanacak! www.com');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 3) {
+   		msg.reply('Görende 3 gün susuz kaldı sanacak! www.com');
+   	}else if (Math.floor((Math.random() * 4) + 1) === 4) {
+   		msg.reply('Biraz Google la! ;)');
+   	}
+  }
+
+  if (msg.content === 'selamın aleyküm') {
+   	msg.reply('ve aleyküm selam');
+  }
+
+  if (msg.content === 'bye bye') {
+   	msg.reply('su gibi git su gibi gel');
+  }
+
+  if (msg.content === 'günaydın') {
+   	msg.reply('sana da günaydın');
+  }
+
+  if (msg.content === 'herkese günaydın') {
+   	msg.reply('yepyeni bir güne merhaba :)');
+  }
+
+  if (msg.content === 'iyi geceler') {
+   	msg.reply('sana da iyi geceler');
+  }
+
+  if (msg.content === 'sa') {
+   	msg.reply('as');
+  }
+
+  if (msg.content === 'iyi akşamlar') {
+   	msg.reply('sana da iyi akşamlar');
+  }
+
+  if (msg.content === 'selamın aleyküm') {
+   	msg.reply('ve aleyküm selam');
+  }
+
+  if (msg.content === 'güle güle') {
+   	msg.reply('sana da güle güle');
+  }
 });
 
-client.elevation = message => {
-  if(!message.guild) {
-	return; }
-  let permlvl = 0;
-  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
-  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-  if (message.author.id === ayarlar.sahip) permlvl = 4;
-  return permlvl;
-};
-
-var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-
-client.on('warn', e => {
-  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
-});
-
-client.on('error', e => {
-  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
-});
-
-client.login(process.env.BOT_TOKEN);
+client.login('TOKEN');
