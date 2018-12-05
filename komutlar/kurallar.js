e37c3c63e85f40efdca3d3bb4bb7194c1a1f79a3
BugraGunes <42942210+BugraGunes@users.noreply.github.com>
2018-12-05 16:31:53 +0200

    Update kurallar.js

--- komutlar/kurallar.js
+++ komutlar/kurallar.js
@@ -14,7 +14,7 @@ exports.run = (client, message) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Bot sürümü: v' + ayarlar.surum + ' Yapımcı: Serhan (Black Monday) **Sohbet ve Oyun**\n\n_**BOTU EKLEMEK İÇİN LİNK:**_\n\nhttps://discordapp.com/oauth2/authorize?client_id=288310817810546699&scope=bot&permissions=401812495 \n\n_**Linkler:**_\n\n**Sohbet ve Oyun** sunucusunun davet linki: https://discord.gg/GEeGjnH \nBotun davet linki: https://discordapp.com/oauth2/authorize?client_id=288310817810546699&scope=bot&permissions=401812495 \n\n**:copyright: 2017 Sohbet ve Oyun**');
+    .setDescription('Bot sürümü: v' + ayarlar.surum + '**WİNTERFALLMC DİSCORD VE MİNECRAFT SUNUCUSU KURALLARI:**\n\n1-Hile, Macro kullanmak yasaktır.\n2- Küfür etmek, argo gelime kullanmak, siyaset ve ırkçılık yapmak yasaktır.\n3-Hile ve bug kullanmak yasaktır.\n4-Kaybolan eşyalarınızdan biz sorumlu değiliz.\n5-Diğer oyunculara güvenmek sizin elinizdedir. Aksi bir olay takdirinde müdahale etmeyiz.\n6-TPA tuzağı yasaktır.\n7-Yetkili taklidi vb. yapmak yasaktır\n8-Reklam yapmak **kesinlikle** yasaktır.\n9-Sunucuya hakaret sebebi mute veya ban sebebidir.\n10-Her çeşit dolandırıcılık yasaktır.\n11-Açık arttırmaya küfür, hakaret içerikli eşyaların konulması yasaktır.\n12-Başka sunucuların ismi kullanmak yasaktır.\n13-Yan hesap veya anlaşmalı bir hesap ile kill kasmak yasaktır.\n14-Size sattığımız edit eşyalar sizin sorumluluğunuzdadır. Kaybolduğu taktirde müdahale etmeyiz.\n15-FPS veya TPS düşüren (lag yapan) sistemler yapmak hem yasaktır, hem de ağır cezalara sebebiyettir. \n\n**Kurallara Uymadığınız Taktirde Gerekli İşlemler Yapılacaktır.**');
     return message.author.sendEmbed(pingozel)
 };
 
@@ -26,7 +26,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'bilgi',
-  description: 'Bot ile ilgili bilgi verir.',
-  usage: 'bilgi'
+  name: 'kurallar',
+  description: 'Sunucu kurallarını gösterir.',
+  usage: 'kurallar'
 };
